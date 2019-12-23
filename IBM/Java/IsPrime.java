fd5d161836b63b3c0d76d0d9091a23b5f9ad0a35
zouppa <zouppa@gmail.com>
2019-12-23 15:19:31 -0600

    java correction

--- IBM/Java/IsPrime.java
+++ IBM/Java/IsPrime.java
@@ -1,27 +1,26 @@
 import com.google.gson.JsonObject;
 
-public class Prime {
+public class IsPrime {
   public static JsonObject main(JsonObject args) {
 
     int number = 2;
     boolean flag = false;
 
     if (args.has("number"))
-      max = args.getAsJsonPrimitive("number").getAsInt();
+      number = args.getAsJsonPrimitive("number").getAsInt();
 
     if (number < 2) {
       flag = true;
     } else {
-
-      {
-        if (num % i == 0) {
+      for (int i = 2; i <= number / 2; ++i) {
+        if (number % i == 0) {
           flag = true;
           break;
         }
       }
     }
 
-    flag =!flag;
+    flag = !flag;
 
     JsonObject response = new JsonObject();
     response.addProperty("payload", flag);
